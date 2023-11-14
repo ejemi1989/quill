c6446284b6b3f084c48fe65493a2d16f69ecc635
Suraju oladimeji <ayodejioladimeji@gmail.com>
2023-11-14 10:37:41 +0000

    Update UploadButton.tsx

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -8,7 +8,7 @@ import {
 } from './ui/dialog'
 import { Button } from './ui/button'
 
-import Dropzone, { useDropzone } from 'react-dropzone'
+import Dropzone from 'react-dropzone'
 import { Cloud, File, Loader2 } from 'lucide-react'
 import { Progress } from './ui/progress'
 import { useUploadThing } from '@/lib/uploadthing'
@@ -58,10 +58,9 @@ const UploadDropzone = ({
 
     return interval
   }
-const { open } = useDropzone()
+
   return (
     <Dropzone
-      noClick={true}
       multiple={false}
       onDrop={async (acceptedFile) => {
         setIsUploading(true)
@@ -98,7 +97,6 @@ const { open } = useDropzone()
       }}>
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
-          onClick={open}
           {...getRootProps()}
           className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
           <div className='flex items-center justify-center h-full w-full'>
